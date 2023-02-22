d3ab8e54fb8c1d5917c1074af4727928e23cea50
GeorgeAnghel114 <georgeanghel114@gmail.com>
2023-02-22 10:07:46 +0200

    [feat] -maxSumOfRow & maxSumOfCol of a matrix

--- school_algo/algo/src/main/java/org/example/Matrix.java
+++ school_algo/algo/src/main/java/org/example/Matrix.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Matrix {
@@ -7,10 +8,12 @@ public class Matrix {
         System.out.println("merge");
         int[][] arr =   {{1,2,3}
                         ,{4,1,6}
-                        ,{7,8,4}};
-        System.out.println(biggestDiagonalNumber(arr));
-        System.out.println(Arrays.deepToString(orderSecondLineInTheMatrix(arr)));
-        System.out.println(Arrays.toString(lineWithMostOddNum(arr)));
+                        ,{7,100,4}};
+//        System.out.println(biggestDiagonalNumber(arr));
+//        System.out.println(Arrays.deepToString(orderSecondLineInTheMatrix(arr)));
+//        System.out.println(Arrays.toString(lineWithMostOddNum(arr)));
+//        System.out.println(Arrays.toString(maxSumOfRow(arr)));
+        System.out.println(Arrays.toString(maxSumOfCol(arr)));
 
     }
 
@@ -63,4 +66,50 @@ public class Matrix {
         }
         return temp;
     }
+
+    public static int[] maxSumOfRow(int[][] arr){
+        int n = arr.length;
+        System.out.println(n);
+        int maxSum = Integer.MIN_VALUE;
+        int idx = -1;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum+=arr[i][j];
+            }
+            if(maxSum<sum){
+                maxSum = sum;
+                idx = i;
+            }
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(idx);
+        res.add(maxSum);
+        System.out.println(res);
+        return new int[]{1};
+    }
+
+
+        public static int[] maxSumOfCol(int[][] arr){
+            int n = arr.length;
+            int maxSum = Integer.MIN_VALUE;
+            int idx = -1;
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 0; j < n; j++) {
+                    sum+= arr[j][i];
+                }
+                if(maxSum<sum){
+                    maxSum = sum;
+                    idx = i;
+                }
+
+            }
+            ArrayList<Integer> res = new ArrayList<>();
+            res.add(idx);
+            res.add(maxSum);
+            System.out.println(res);
+            return new int[]{1};
+
+        }
 }
